f7a95d41160fd0c821d9fef8775cb09ff748a32e
kalerianozamatov-beep <kalerianozamatov@gmail.com>
2026-06-10 04:51:44 +0300

    Initialize bot with aiogram and start command

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
-print("Бот запущен!")
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+
+TOKEN = "8801250152:AAGpx33YIM0qQO2zxSyLTY2bcvlBBCSokXI"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: Message):
+    await message.answer(
+        "🏙 Добро пожаловать в CityBot!\n\nНапиши: Профиль"
+    )
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
